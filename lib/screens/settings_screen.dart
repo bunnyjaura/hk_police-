266459de7f0724bf565e043232cccd169fdbf2d4
Diksha <dishash719@gmail.com>
2023-05-12 15:33:57 +0530

    diksha

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:hk_policestation_hq/API.dart';
 import 'package:hk_policestation_hq/controllers/Controllers.dart';
 import 'package:hk_policestation_hq/screens/settings/about.dart';
@@ -7,6 +8,7 @@ import 'package:hk_policestation_hq/screens/settings/help.dart';
 import 'package:hk_policestation_hq/screens/settings/manage_police_station.dart';
 import 'package:hk_policestation_hq/screens/settings/profile_screen.dart';
 import 'package:hk_policestation_hq/screens/settings/terms.dart';
+import 'package:hk_policestation_hq/screens/splashScreen.dart';
 
 import '../widgets/settings_item.dart';
 
@@ -28,41 +30,40 @@ class SettingsScreen extends StatelessWidget {
     Icons.confirmation_num,
     Icons.logout,
   ];
-  List<void Function()?> ontaps = [
-    () {
-      Get.to(ProfileScreen());
-    },
-    () {
-      Get.to(ProfileScreen());
-    },
-    () {
-      Get.to(ProfileScreen());
-    },
-    () {
-      Get.to(ProfileScreen());
-    },
-    () {
-      Get.to(ProfileScreen());
-    },
-    () {
-      Get.to(ProfileScreen());
-    },
-    // Get.to(ManagePoliceStation()),
-    // Get.to(AboutScreen()),
-    // Get.to(HelpScreen()),
-    // Get.to(TermsScreen()),
-    // Api().logout().whenComplete(() => null).then((value) {
-    //   print(value);
-    // }
-    // )
-  ];
+
   Map i = {
     0: {
-      "nmae": "kok",
+      "name": "My Profile",
+      "icon": Icons.person,
+      "to": const ProfileScreen(),
+    },
+    1: {
+      "name": "Manage Police station ",
+      "icon": Icons.local_post_office,
+      "to": const ManagePoliceStation(),
+    },
+    2: {
+      "name": "About",
+      "icon": Icons.note_add,
+      "to": const AboutScreen(),
+    },
+    3: {
+      "name": "Help",
       "icon": Icons.help,
-      "onTAp": Get.to(ProfileScreen()),
-    }
+      "to": const HelpScreen(),
+    },
+    4: {
+      "name": "Terms & Conditions",
+      "icon": Icons.confirmation_num,
+      "to": const TermsScreen(),
+    },
+    5: {
+      "name": "Logout",
+      "icon": Icons.logout,
+      "to": const ProfileScreen(),
+    },
   };
+  final box = GetStorage();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,11 +71,14 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.arrow_back_ios,
-          color: Colors.black,
+        leading: InkWell(
+          onTap: () => Get.to('Homescreen'),
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
         ),
-        title: Center(
+        title: const Center(
             child: Text(
           'Settings',
           style: TextStyle(
@@ -95,15 +99,17 @@ class SettingsScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Container(
+            SizedBox(
               height: 50,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
                     child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/ic_hospital.png',),
+                      backgroundImage: AssetImage(
+                        'assets/ic_hospital.png',
+                      ),
                       radius: 20,
                     ),
                   ),
@@ -112,28 +118,39 @@ class SettingsScreen extends StatelessWidget {
                     children: [
                       Text(
                         '${Controllers().userFetchData.name}',
-                        style: TextStyle(
+                        style: const TextStyle(
                             fontSize: 22, fontWeight: FontWeight.w600),
                       ),
                       Text('${Controllers().userFetchData.address}',
-                          style: TextStyle(
+                          style: const TextStyle(
                               fontSize: 15, fontWeight: FontWeight.w100)),
                     ],
                   ),
                 ],
               ),
             ),
-            Flexible(
+            SizedBox(
+              height: 300,
               child: ListView.builder(
-                itemCount: 6,
+                itemCount: 5,
                 itemBuilder: (BuildContext context, int index) {
                   return SettingsItem(
-                      name: names[index],
-                      icon: icons[index],
-                      ontap: () => ontaps[index]);
+                    name: names[index],
+                    icon: icons[index],
+                    ontap: () => Get.to(i[index]["to"]),
+                  );
                 },
               ),
             ),
+            SettingsItem(
+                name: i[5]['name'],
+                icon: i[5]['icon'],
+                ontap: () {
+                  Api().logout().whenComplete(() => null).then((value) {
+                    box.erase();
+                    Get.offAll(const SplashScreen());
+                  });
+                })
           ],
         ),
       ),
